79666bc017e2977c7a88ec564635024f4417fb6e
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-08-27 19:44:40 +0200

    Using Factory Beans

--- src/main/java/guru/springframework/service/PrimaryUkrainianGreetingService.java
+++ src/main/java/guru/springframework/service/PrimaryUkrainianGreetingService.java
@@ -1,12 +1,5 @@
 package guru.springframework.service;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
-@Profile("ua")
 public class PrimaryUkrainianGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
