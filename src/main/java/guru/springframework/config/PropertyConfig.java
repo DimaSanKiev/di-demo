8f6e13c5c8b5374c67cd5d9db660a8fb53bbec97
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-08-29 21:44:42 +0200

    Add Spring environment properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,29 +1,35 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
+    // Use Environment variable USERNAME=MyOVERRIDE!
+    @Autowired
+    private Environment env;
+
     @Value("${guru.username}")
-    String user;
+    private String user;
 
     @Value("${guru.password}")
-    String password;
+    private String password;
 
     @Value("${guru.dburl}")
-    String url;
+    private String url;
 
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
