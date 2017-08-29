9f338cbfdafe7aba8ce37c770eae7b18078d2864
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-08-29 22:10:54 +0200

    Using Spring Boot application.properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,26 +2,12 @@ package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    // Use Environment variable USERNAME=MyOVERRIDE!
-    @Autowired
-    private Environment env;
 
     @Value("${guru.username}")
     private String user;
@@ -44,7 +30,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource dataSource = new FakeDataSource();
-        dataSource.setUser(env.getProperty("USERNAME"));
+        dataSource.setUser(user);
         dataSource.setPassword(password);
         dataSource.setUrl(url);
         return dataSource;
@@ -59,8 +45,4 @@ public class PropertyConfig {
         return jmsBroker;
     }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
 }
